2c402f88783ee554cf1fb749fbf8bcb670a374f6
anas-alnaser <anasalnaser24@gmail.com>
2025-12-09 10:36:49 +0300

    add .env file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,13 +10,13 @@ import { getAuth, Auth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCYe5xXLM0DBUsf6Ru1DjyIfC2Zuq-A0t0",
-  authDomain: "anas-9f395.firebaseapp.com",
-  projectId: "anas-9f395",
-  storageBucket: "anas-9f395.firebasestorage.app",
-  messagingSenderId: "375882098172",
-  appId: "1:375882098172:web:1e2c6977645b3794173e79",
-  measurementId: "G-DK0E6GSLT0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
